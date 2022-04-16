bcd4325f41d2fa2e82959de6db272a73ec9a73b9
danieldamian09 <humbertorivero9@gmail.com>
2022-04-16 16:25:37 -0300

    create context and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import {Container, Grid, Typography} from "@mui/material";
 import Formulario from "./components/Formulario";
+import {NoticiasProvider} from "./context/NoticiasProvider";
 
 function App() {
 	return (
-		<Container>
-			<header>
-				<Typography align="center" marginY={5} component="h1" variant="h3">
-					Buscador de Noticias
-				</Typography>
-			</header>
-			<Grid container direction="row" justifyContent="center" alignItems="center" >
-				<Grid item md={6} xs={12} >
-					<Formulario />
+		<NoticiasProvider>
+			<Container>
+				<header>
+					<Typography align="center" marginY={5} component="h1" variant="h3">
+						Buscador de Noticias
+					</Typography>
+				</header>
+				<Grid
+					container
+					direction="row"
+					justifyContent="center"
+					alignItems="center"
+				>
+					<Grid item md={6} xs={12}>
+						<Formulario />
+					</Grid>
 				</Grid>
-			</Grid>
-		</Container>
+			</Container>
+		</NoticiasProvider>
 	);
 }
 
