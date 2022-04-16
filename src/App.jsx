0abdc7317c7f5609e2f2637985e19f5a0dfd9619
danieldamian09 <humbertorivero9@gmail.com>
2022-04-16 15:18:11 -0300

    create form

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import {Container, Grid, Typography} from "@mui/material"
+import {Container, Grid, Typography} from "@mui/material";
+import Formulario from "./components/Formulario";
 
 function App() {
-
-  return (
-    <Container>
-      <header>
-        <Typography align="center" marginY={5} component="h1" variant="h3">
-          Buscador de Noticias
-        </Typography>
-      </header>
-    </Container>
-  )
+	return (
+		<Container>
+			<header>
+				<Typography align="center" marginY={5} component="h1" variant="h3">
+					Buscador de Noticias
+				</Typography>
+			</header>
+			<Grid container direction="row" justifyContent="center" alignItems="center" >
+				<Grid item md={6} xs={12} >
+					<Formulario />
+				</Grid>
+			</Grid>
+		</Container>
+	);
 }
 
-export default App
+export default App;
