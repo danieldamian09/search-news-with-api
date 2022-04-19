cebb6c3ef5fcb7f23acf24101ee1fd2a86bfa7f1
danieldamian09 <humbertorivero9@gmail.com>
2022-04-18 21:45:45 -0300

    adding pagination

--- src/components/ListadoNoticias.jsx
+++ src/components/ListadoNoticias.jsx
@@ -1,21 +1,33 @@
-import { Grid, Typography } from "@mui/material";
+import {Grid, Typography} from "@mui/material";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 import Noticia from "./Noticia";
-import useNoticias from "../hook/useNoticias"
+import useNoticias from "../hook/useNoticias";
 
 const ListadoNoticias = () => {
+	const {noticias} = useNoticias();
 
-  const { noticias } = useNoticias();
+	return (
+		<>
+			<Typography textAlign={"center"} marginY={5} component="h2" variant="h3">
+				Últimas Noticias
+			</Typography>
+			<Grid container spacing={2}>
+				{noticias.map((noticia) => (
+					<Noticia key={noticia.url} noticia={noticia} />
+				))}
+			</Grid>
+			<Stack
+				spacing={2}
+				sx={{marginY: 5}}
+				direction={"row"}
+				justifyContent={"center"}
+				alignItems={"center"}
+			>
+				<Pagination count={10} color="primary" />
+			</Stack>
+		</>
+	);
+};
 
-  return (
-    <>
-      <Typography textAlign={"center"} marginY={5} component="h2" variant="h3">Últimas Noticias</Typography>
-      <Grid container spacing={2}>
-        {noticias.map(noticia => (
-          <Noticia key={noticia.url} noticia={noticia} />
-        ))}
-      </Grid>
-    </>
-  )
-}
-
-export default ListadoNoticias
+export default ListadoNoticias;
--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -7,12 +7,15 @@ const NoticiasProvider = ({ children }) => {
   
   const [categoria, setCagoria] = useState("general");
   const [noticias, setNoticias] = useState([]);
+  const [paginas, setPaginas] = useState(1);
+  const [totalNoticias, setTotalNoticias] = useState(0);
 
   const consultarApi = async () => {
     try {
       const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`;
       const { data } = await axios(url);
       setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
     } catch (error) {
       return(error);
     }
